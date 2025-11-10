1c82a475b4c115ba5b7d9c2322fbc9b449ea83d6
Sena <antoniocarlossenajunior@gmail.com>
2025-11-09 21:11:56 -0300

    feat: UPDATE PROFILE

--- app/schema/user.py
+++ app/schema/user.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, EmailStr
 from datetime import datetime
-
+from typing import Optional
 
 class UserBase(BaseModel):
     email: EmailStr
@@ -10,11 +10,21 @@ class UserBase(BaseModel):
 class UserCreate(UserBase):
     password: str
 
+class UserProfileUpdate(BaseModel):
+    full_name: Optional[str] = None
+    description: Optional[str] = None
+    city: Optional[str] = None
+    state: Optional[str] = None
+    profile_pic_url: Optional[str] = None
 
 class UserResponse(UserBase):
     id: str
     is_active: bool
     created_at: datetime
 
+    description: Optional[str] = None
+    city: Optional[str] = None
+    state: Optional[str] = None
+    profile_pic_url: Optional[str] = None
     class Config:
         from_attributes = True
